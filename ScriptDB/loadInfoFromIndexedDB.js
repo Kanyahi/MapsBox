f0d6615d1a5dfd515201f2ab320c58d584a16735
Hristo Chilov <97700326+Kanyahi@users.noreply.github.com>
2026-07-27 07:21:54 +0300

    Add async function to load data from IndexedDB

--- ScriptDB/loadInfoFromIndexedDB.js
+++ ScriptDB/loadInfoFromIndexedDB.js
@@ -1,1 +1,31 @@
+async function getTable(tableName) {
 
+    return new Promise((resolve, reject) => {
+
+        const request = indexedDB.open("MapsBoxDB");
+
+        request.onsuccess = (e) => {
+
+            const db = e.target.result;
+
+            const store = db
+                .transaction(tableName, "readonly")
+                .objectStore(tableName);
+
+            const req = store.get("data");
+
+            req.onsuccess = () => {
+
+                resolve(req.result ? req.result.rows : []);
+
+            };
+
+            req.onerror = () => reject(req.error);
+
+        };
+
+        request.onerror = () => reject(request.error);
+
+    });
+
+}
